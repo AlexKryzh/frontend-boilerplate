bced86220cfb95c791122817a9d24f275b8dbb71
alexk-restoin <a.kryzhanovskyy@resto-in.com>
2016-09-06 10:39:07 +0200

    project configuration

---
+++ gulpfile.ts
@@ -0,0 +1,36 @@
+// /// <reference path="./typings/index.d.ts"/>
+// /// <reference path="./typings.d.ts"/>
+
+// import { readdirSync } from 'fs';
+// import * as gulp from 'gulp';
+// import * as gulpHelp from 'gulp-help';
+// import * as util from 'gulp-util';
+// import * as browserSync from 'browser-sync';
+// import * as configuration from './tools/config';
+// import * as gulpLoadPlugins from 'gulp-load-plugins';
+
+// const plugins = gulpLoadPlugins({camelize: true});
+// const CacheBuster = require('gulp-cachebust');
+// const del = require('del');
+
+// global.tools = {};
+
+// global.tools.gulp = gulpHelp(gulp, {description: '', hideEmpty: true});
+// global.tools.plugin = plugins;
+// global.tools.prod = false;
+// global.tools.mocks = util.env.mocks;
+// global.tools.config = configuration.config;
+// global.tools.bs = browserSync.create();
+// global.tools.cachebust = new CacheBuster();
+// global.tools.del = del;
+// global.tools.timestamp = Math.round(Date.now()/1000);
+
+// //Register tasks from tasks folder
+// const tasks:string[] = readdirSync('./tools/tasks/').filter((task) => {
+//     //filter just ts files
+//     return task.indexOf('.ts') !== -1;
+// });
+
+// tasks.forEach((task) => {
+//     require('./tools/tasks/' + task);
+// });
