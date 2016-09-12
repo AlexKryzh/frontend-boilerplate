be646a0d3ba78f8c10863928d44c54cd726a9fd9
alexk-restoin <a.kryzhanovskyy@resto-in.com>
2016-09-12 14:57:45 +0200

    gulp typescript tasks fix

--- tools/tasks/scripts.ts
+++ tools/tasks/scripts.ts
@@ -2,9 +2,21 @@ var $ = global.tools;
 
 $.gulp.task('scripts', 'Process scripts files', () =>{
 
-    let tsProject = $.plugin.typescript.createProject('tsconfig.json');
+    let tsProject = $.plugin.typescript.createProject('src/tsconfig.json', {
+        declaration: true,
+        noExternalResolve: true
+    });
 
-    return $.gulp.src('src/scripts/**/*.ts')
-        .pipe($.plugin.typescript(tsProject))
-        .pipe($.gulp.dest('dist/scripts/'));
+    let tsResult = tsProject.src()
+    .pipe($.plugin.typescript(tsProject));
+ 
+    return tsResult.js.pipe($.gulp.dest('dist/scripts/'));
+
+    // return $.gulp.src()   //'./src/scripts/**/*.ts'
+    //     .pipe($.plugin.tslint({
+    //         formatter: 'verbose'
+    //     }))
+    //     .pipe($.plugin.tslint.report())
+    //     .pipe($.plugin.typescript(tsProject))
+    //     .pipe($.gulp.dest('dist/scripts/'));
 });
