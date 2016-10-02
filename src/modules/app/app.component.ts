26051b7531a353f329d806009b3e4668dd5a1534
AlexKryzh <alex@kryzh.com>
2016-10-02 22:29:15 +0200

    language change functionality

--- src/modules/app/app.component.ts
+++ src/modules/app/app.component.ts
@@ -11,7 +11,6 @@ export class AppComponent {
     constructor(private translate: TranslateService){
         this.getLanguages();
         translate.addLangs(this.languages);
-        // this language will be used as a fallback when a translation isn't found in the current language
         translate.setDefaultLang(this.languages[1]);
 
          let browserLang = translate.getBrowserLang();
