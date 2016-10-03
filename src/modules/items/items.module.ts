82a9328566f4bf74a158584b0440528bea5cd20c
alexk-restoin <a.kryzhanovskyy@resto-in.com>
2016-10-03 18:04:24 +0200

    item component added

--- src/modules/items/items.module.ts
+++ src/modules/items/items.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ItemsService } from './items.service';
 import { ItemsComponent } from './items.component';
+import { ItemComponent } from './item.component';
 import { SharedModule } from '../shared/shared.module';
 
 import { ItemsRouting } from './items.routing';
 
 @NgModule({
     imports: [ ItemsRouting, SharedModule ],
-    declarations: [ ItemsComponent ],
+    declarations: [ ItemsComponent, ItemComponent ],
     providers: [ ItemsService ],
-    exports: [ ItemsComponent, SharedModule]
+    exports: [ ItemsComponent, ItemComponent, SharedModule]
 })
 export class ItemsModule { }
