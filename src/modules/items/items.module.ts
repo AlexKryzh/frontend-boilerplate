363355716095def9f12193b832e6210247ceef9a
AlexKryzh <alex@kryzh.com>
2016-10-16 21:38:06 +0200

    filter componend removed, logic moved to items component

--- src/modules/items/items.module.ts
+++ src/modules/items/items.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ItemsService } from './items.service';
 import { ItemsComponent } from './items.component';
-import { FiltersComponent } from './filters.component';
 import { ItemComponent } from './item.component';
 import { SharedModule } from '../shared/shared.module';
 
@@ -9,7 +8,7 @@ import { ItemsRouting } from './items.routing';
 
 @NgModule({
     imports: [ ItemsRouting, SharedModule ],
-    declarations: [ ItemsComponent, FiltersComponent, ItemComponent ],
+    declarations: [ ItemsComponent, ItemComponent ],
     providers: [ ItemsService ],
     exports: [ ]
 })
