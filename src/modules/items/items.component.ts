2278e89347db061098b7bbc89d53bcea93fe6fbd
alexk-restoin <a.kryzhanovskyy@resto-in.com>
2016-10-25 15:16:27 +0200

    price sort fixed

--- src/modules/items/items.component.ts
+++ src/modules/items/items.component.ts
@@ -107,9 +107,25 @@ export class ItemsComponent {
         let sort = this.options.sort;
         if(this.options.sort.expression){
             this.ResultItems.sort(function(a: any, b: any){
-                let x = a[sort.expression].toLowerCase();
-                let y = b[sort.expression].toLowerCase();
-                return x < y ? -1 : x > y ? 1 : 0;
+                if(typeof a[sort.expression] !== typeof b[sort.expression]){
+                    a[sort.expression] = a[sort.expression].toString();
+                    b[sort.expression] = b[sort.expression].toString();
+                }
+                var result : any;
+                switch(typeof a[sort.expression]){
+                    case 'number':
+                        result = a[sort.expression] - b[sort.expression];
+                        break;
+                    case 'string':
+                        let x = a[sort.expression].toLowerCase();
+                        let y = b[sort.expression].toLowerCase();
+                        result = x < y ? -1 : x > y ? 1 : 0;
+                        break;
+                    default:
+                        result = 0;
+                        break;
+                }
+                return result;
             });
         }else{
             //default sort
